3607d192f66469ad2bf0c4a99bf18eb09f8979e8
supergijs <info@supergijs.com>
2023-03-19 15:30:47 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Make the DIV element draggable:
+// var dragElements = document.getElementsByClassName('dragable');
+// for(dragThing of dragElements){
+// dragElement(dragThing);
 dragElement(document.getElementById("signs"));
 
 function dragElement(elmnt) {
@@ -70,8 +73,6 @@ function clarity() {
 
 // MOVE PLANT WITH SLIDERS
 
-  // link card to plant
-
   // cardSliders.addEventListener('change', e =>)
   var plants = document.getElementsByClassName("plant");
 
@@ -102,29 +103,6 @@ function clarity() {
       });    
 
     }
-
-    // var controlValue = controlSlider.value;
-
-    // careSlider.addEventListener('input', e => {
-    //     var careLevel = e.target.value;
-    //     // console.log(careLevel);
-
-    //     // plant.style.left = careLevel;
-    //   });    
-
-    // for(slider of cardSliders){
-
-    // }
-    // var care = cardSliders[];
-    // console.log(care);
-
-    // cardSliders[0].addEventListener('change', e => {
-    //   // console.log(cardSliders[0]);
-
-    //   var careLevel = e.target.value;
-      
-    //   plant.style.left = careLevel;
-    // });    
 
   }    
 
@@ -185,3 +163,38 @@ for(toggle of toggles) {
     };
   });
 }
+
+// Lines between plants
+
+// function getOffset( el ) {
+//   var rect = el.getBoundingClientRect();
+//   return {
+//       left: rect.left + window.pageXOffset,
+//       top: rect.top + window.pageYOffset,
+//       width: rect.width || el.offsetWidth,
+//       height: rect.height || el.offsetHeight
+//   };
+// }
+
+// function connect(div1, div2, color, thickness) { // draw a line connecting elements
+//   var off1 = getOffset(div1);
+//   var off2 = getOffset(div2);
+//   // bottom right
+//   var x1 = off1.left + off1.width;
+//   var y1 = off1.top + off1.height;
+//   // top right
+//   var x2 = off2.left + off2.width;
+//   var y2 = off2.top;
+//   // distance
+//   var length = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+//   // center
+//   var cx = ((x1 + x2) / 2) - (length / 2);
+//   var cy = ((y1 + y2) / 2) - (thickness / 2);
+//   // angle
+//   var angle = Math.atan2((y1-y2),(x1-x2))*(180/Math.PI);
+//   // make hr
+//   var htmlLine = "<div style='padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);' />";
+//   //
+//   // alert(htmlLine);
+//   document.body.innerHTML += htmlLine;
+// }
