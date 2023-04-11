455d0589f5c5a57379ac0da26f3a4e8cd6e4db4a
supergijs <info@supergijs.com>
2023-04-11 16:56:34 +0200

    nature and world update 2?

--- script.js
+++ script.js
@@ -1,76 +1,3 @@
-// Make the DIV element draggable:
-// var dragElements = document.getElementsByClassName('dragable');
-// for(dragThing of dragElements){
-// dragElement(dragThing);
-dragElement(document.getElementById("signs"));
-
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } 
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-// Drag with touch? Still doesn't work
-window.onload = function() {
-  var box = document.getElementById('signs');  
-  box.addEventListener('touchmove', function(e) {
-    // grab the location of touch
-    var touchLocation = e.targetTouches[0];
-    // assign box new coordinates based on the touch.
-    box.style.left = touchLocation.pageX + 'px';
-    box.style.top = touchLocation.pageY + 'py';
-  })
-  box.addEventListener('touchend', function(e) {
-    // current box position.
-    var x = parseInt(box.style.left);
-    var y = parseInt(box.style.top);
-  })
-  
-}
-
-// High contrast button;
-function clarity() {
-    var element = document.body;
-    element.classList.toggle("black");
-    var ground = document.getElementById('ground');
-    ground.classList.toggle("groundless");
-    var nav = document.getElementById('signs');
-    nav.classList.toggle("green");
-    var btn = document.getElementById('clarity');
-    btn.classList.toggle("on");
-}
-
 // MOVE PLANT WITH SLIDERS
 
   // cardSliders.addEventListener('change', e =>)
@@ -110,7 +37,7 @@ function clarity() {
   // style plant
 
 // FAMILY HIGHLIGHT & TOGGLE
-var famToggles = document.getElementById("toggles");
+var famToggles = document.getElementById("signs");
 var toggles = famToggles.getElementsByTagName("label"); 
 var families = document.getElementsByClassName("plants");
 
